09f01a12ddfc8c2258d971186bf3a83c039f9ec4
trantanthanh <trantanthanh@gmail.com>
2023-07-14 22:04:41 +0700

    add Player Driver, learn [SerilizeField] Transform, Time.deltaTime

---
+++ Assets/Driver.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float steerSpeed = 80f;
+    [SerializeField] float moveSpeed = 10f;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float speedAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, speedAmount, 0);
+    }
+}
