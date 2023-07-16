f5749dbcf1625bbb01a51c302fa2c5c4bb98c9e1
trantanthanh <trantanthanh@gmail.com>
2023-07-16 12:32:07 +0700

    add collectable, destroy object, add tag

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
+
+    private bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.5f;
     private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch !!!");
@@ -16,6 +19,17 @@ public class Collision : MonoBehaviour
     /// <param name="other">The other Collider2D involved in this collision.</param>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger");
+        if (other.tag == "Package" && !hasPackage)
+        {
+            hasPackage = true;
+            Debug.Log("collect package");
+            Destroy(other.gameObject, destroyDelay);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            hasPackage = false;
+            Debug.Log("Package delivered");
+        }
     }
 }
